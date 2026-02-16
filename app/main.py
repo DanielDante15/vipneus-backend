261fff28c1c1280e80c497003c95cfcebc0fbb7c
Daniel Macedo <daniel.macedo@br.bosch.com>
2026-02-15 21:45:46 -0300

    add localhost only for tests

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Em produção, especifique os domínios
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
